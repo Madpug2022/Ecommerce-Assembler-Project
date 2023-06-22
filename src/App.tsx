093e8f92462ca1b96d4108307139ee12ca5ef26c
Matias Alaimo <matias.chiappa@hotmail.com>
2023-06-22 12:01:11 +0200

    Added feature: added notifications

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import './styles/App.css'
 import Header from './layout/Header'
 import Footer from './layout/Footer'
 import { ShopContextProvider } from './context/ShopContext'
+import { ReactNotifications } from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
 
 function App() {
   return (
     <>
+    <ReactNotifications />
     <ShopContextProvider>
+
       <Header/>
       <Footer/>
     </ShopContextProvider>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Product } from "../interfaces/product";
 import '../styles/components/ProductCard.styles.css'
 import { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
+import { Store } from 'react-notifications-component';
 function ProductCard(props: Product){
     const {productId, name, price, img} = props;
     const shopContext = useContext(ShopContext);
@@ -9,6 +10,19 @@ function ProductCard(props: Product){
         return null;
     }
     const { addToCart } = shopContext;
+    const notify = () => {
+        Store.addNotification({
+            title: "Product added to cart",
+            type: "info",
+            insert: "top",
+            container: "top-right",
+            animationIn: ["animate__animated", "animate__fadeIn"],
+            animationOut: ["animate__animated", "animate__fadeOut"],
+            dismiss: {
+              duration: 3000,
+            }
+          });
+    }
     return (
         <div className="product-card" id={productId}>
             <div className="pc-top">
@@ -17,7 +31,7 @@ function ProductCard(props: Product){
             <div className="pc-bottom">
                 <h3 className="fProd-title">{name}</h3>
                 <p className="fProd-price">{price.toString()} €</p>
-                <button onClick={() => addToCart(productId)} className="fProd-button">Add to cart</button>
+                <button onClick={() => {addToCart(productId); notify()}}  className="fProd-button">Add to cart</button>
             </div>
 
         </div>
